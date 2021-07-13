8c7ccb1d5cac8760e27924d3913ed2d4fd54d4a4
Luiz Gustavo Freitas Pereira <luizpereira.mec@outlook.com>
2021-07-13 17:50:38 -0300

    Curso 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro/";
+import ListaDeCategorias from "./components/ListaDeCategorias";
 import "./assets/App.css";
-import './assets/index.css';
+import "./assets/index.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.notas = [];
+    this.state = {
+      notas: [],
+    };
+  }
 
-function App() {
-  return (
-    <section className="conteudo">
-      <FormularioCadastro />
-      <ListaDeNotas />
-    </section>
-  );
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias />
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}
+          />
+        </main>
+      </section>
+    );
+  }
 }
 
 export default App;
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -3,13 +3,18 @@ import CardNota from "../CardNota/CardNota";
 import "./estilo.css";
 
 export default class ListaDeNotas extends Component {
+
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Trabalho", "Trabalho", "Estudo").map((categoria, index) => {
+        {this.props.notas.map((nota, index) => {
           return(
             <li key={index} className="lista-notas_item">
-              <CardNota />
+              <CardNota 
+              indice = {index}
+              titulo={nota.titulo} 
+              texto={nota.texto} 
+              apagarNota={this.props.apagarNota}/>
             </li>
           );
         })}
